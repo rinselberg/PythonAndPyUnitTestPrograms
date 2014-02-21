2e3f2ab3f6147f956deaa6dd6a27a9ad17f2f54f
Ronald Inselberg <rinselberg@comcast.net>
2014-02-20 20:53:49 -0800

    Delete currency_exchange_rates.py

--- currency_exchange_rates.py
+++
@@ -1,4 +0,0 @@
-USD_to_SAR = 3.750484           # currency exchange rate
-SAR_to_USD = 1 / USD_to_SAR     # reverse exchange rate
-USD_to_GBR = 0.637050           # currency exchange rate
-GBR_to_USD = 1 / USD_to_GBR     # reverse exchange rate
